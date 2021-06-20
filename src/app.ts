71943b92cb526e3a060e1b9e1ddaf6df9cf302e1
pmNiko <nikolas090189@gmail.com>
2021-06-20 17:52:10 -0300

    Crud api rest finish

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,28 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import swaggerUI from 'swagger-ui-express'
+import swaggerJsDoc from "swagger-jsdoc";
+import { options } from './swaggerOptions'
 import tasksRoutes from './routes/tasks.routes'
+
 
 const app = express()
 
 // utiliza la variable de entorno en caso exista
 app.set('port', process.env.PORT || 3000)
 
-app.use(cors())                //comunicación con otros server
-app.use(morgan('dev'))
-app.use(express.json())
+const specs = swaggerJsDoc(options);
+
+// middlewares
+app.use(cors())
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 
 app.use('/tasks', tasksRoutes)
+
 
 export default app
 
